257471df919581b0cab742cfa99ed3d6a2c31727
Oliver <oliver@giertz.biz>
2025-02-12 19:11:54 +0100

    EntryView auf DropDown Liste umgestellt Datei ExpenseCategory+CoreDataProperties umbenannt

--- CamperLogBook/EntryView.swift
+++ CamperLogBook/EntryView.swift
@@ -1,39 +1,55 @@
 import SwiftUI
 
 enum EntryType: String, CaseIterable, Identifiable {
-    case fuel = "Tankbeleg"
-    case gas = "Gaskosten"
-    case service = "Ver-/Entsorgung"
-    case other = "Sonstige Kosten"
+    case tanken = "Tanken"
+    case gas = "Gas"
+    case service = "Ver- / Entsorgung"
+    case sonstiges = "Sonstiges"
     
     var id: String { self.rawValue }
 }
 
 struct EntryView: View {
-    @State private var selectedEntry: EntryType = .fuel
+    @State private var selectedEntry: EntryType = .tanken
     
     var body: some View {
         NavigationView {
-            VStack {
+            VStack(alignment: .leading) {
+                Text("Kosten Typ auswählen:")
+                    .font(.headline)
+                    .padding(.top, 16)
+                    .padding(.horizontal)
+                
                 Picker("Eintragstyp", selection: $selectedEntry) {
                     ForEach(EntryType.allCases) { type in
-                        Text(type.rawValue).tag(type)
+                        Text(type.rawValue)
+                            .font(.system(size: 28, weight: .bold))
+                            .tag(type)
                     }
                 }
-                .pickerStyle(SegmentedPickerStyle())
-                .padding()
+                .pickerStyle(MenuPickerStyle())
+                .padding(.horizontal)
+                .padding(.vertical, 8)
+                
+                Divider()
+                    .padding(.horizontal)
+                    .padding(.bottom, 8)
                 
                 // Je nach Auswahl den entsprechenden Eingabeformular anzeigen
-                switch selectedEntry {
-                case .fuel:
-                    FuelEntryForm()
-                case .gas:
-                    GasEntryForm()
-                case .service:
-                    ServiceEntryForm()
-                case .other:
-                    OtherEntryForm()
+                Group {
+                    switch selectedEntry {
+                    case .tanken:
+                        FuelEntryForm()
+                    case .gas:
+                        GasEntryForm()
+                    case .service:
+                        ServiceEntryForm()
+                    case .sonstiges:
+                        OtherEntryForm()
+                    }
                 }
+                .padding(.horizontal)
+                
                 Spacer()
             }
             .navigationTitle("Kosten Eintrag")
